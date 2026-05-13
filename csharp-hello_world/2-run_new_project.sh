45b1e885fd25266bd5cf3f40706a0f42b13f2cb6
Emmanuella00 <e.ikirezi@alustudent.com>
2026-05-13 12:28:57 +0200

    fix Hello World output

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-dotnet new console -o 2-run_new_project
-dotnet build 2-run_new_project/
-dotnet run --project 2-run_new_project/
+rm -rf 2-new_project
+dotnet new console -o 2-new_project
+sed -i 's/Hello, World!/Hello World!/g' 2-new_project/Program.cs
+dotnet build 2-new_project/
+dotnet run --project 2-new_project/
