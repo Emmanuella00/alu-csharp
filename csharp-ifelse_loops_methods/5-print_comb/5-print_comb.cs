49a74e1dcba17ce72e47fd86d29dfab6354cac23
Emmanuella00 <e.ikirezi@alustudent.com>
2026-05-17 15:02:28 +0200

    add 5-print comb project

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            if (i == 99)
+            {
+                Console.Write($"{i:D2}\n");
+            }
+            else
+            {
+                Console.Write($"{i:D2}, ");
+            }
+        }
+    }
+}
