92220c7645fd00a5df5c9820ae03d28f1ff24d1d
Emmanuella00 <e.ikirezi@alustudent.com>
2026-06-01 10:09:22 +0200

    add 0-print_array project

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int?[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+        return newArray;
+    }
+}
