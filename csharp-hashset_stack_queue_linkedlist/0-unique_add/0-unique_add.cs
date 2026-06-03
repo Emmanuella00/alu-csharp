63bf363b65adf9fa41a643fa0bed3cab7e4d20c4
Emmanuella00 <e.ikirezi@alustudent.com>
2026-06-03 10:05:09 +0200

    all projects

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        // HashSet removes duplicates automatically
+        HashSet<int> unique = new HashSet<int>(myList);
+
+        int sum = 0;
+        foreach (int num in unique)
+            sum += num;
+
+        return sum;
+    }
+}
---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        // 1. Print number of items
+        Console.WriteLine("Number of items: " + aQueue.Count);
+
+        // 2. Print first item or empty message
+        if (aQueue.Count > 0)
+            Console.WriteLine("First item: " + aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+
+        // 3. Add new item
+        aQueue.Enqueue(newItem);
+
+        // 4. Print if queue contains search
+        bool contains = aQueue.Contains(search);
+        Console.WriteLine($"Queue contains \"{search}\": {contains}");
+
+        // 5. If contains, remove items up to and including search
+        if (contains)
+        {
+            Queue<string> temp = new Queue<string>();
+
+            string dequeued;
+            do
+            {
+                dequeued = aQueue.Dequeue();  // allowed once per loop
+            }
+            while (dequeued != search);
+        }
+
+        return aQueue;
+    }
+}
