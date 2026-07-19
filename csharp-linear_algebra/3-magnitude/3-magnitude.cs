52e77081fc2284875439d56e5c3d1ea861340b0e
Emmanuella00 <e.ikirezi@alustudent.com>
2026-07-19 22:11:45 +0200

    add 3-magnitude

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>Provides vector math operations.</summary>
+public class VectorMath
+{
+    /// <summary>Calculates the magnitude (length) of a 2D or 3D vector.</summary>
+    /// <param name="vector">The vector as an array of doubles.</param>
+    /// <returns>
+    /// The magnitude rounded to the nearest hundredth,
+    /// or -1 if the vector is not 2D or 3D.
+    /// </returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+            return -1;
+
+        double sumOfSquares = 0;
+        foreach (double component in vector)
+        {
+            sumOfSquares += component * component;
+        }
+
+        double magnitude = Math.Sqrt(sumOfSquares);
+        return Math.Round(magnitude, 2);
+    }
+}
